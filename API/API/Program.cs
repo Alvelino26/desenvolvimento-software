577a3dfcb8ecee5473526d3e398773cb4d363476
Gabriel Avelino <alvelinoga@gmail.com>
2024-09-09 16:52:04 -0300

    Testezinho pós Aula

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,18 @@
+//minimal apis em C#
+//testar a API
+// - Rest Client - Extensão VS code
+// - Postman
+// - Insomnia
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Endpoints - Funcionalidades
+// Requisição - URL e metodo/verbo HTTP
+app.MapGet("/", () => "UM SENTIMENTO QUE EU CHAMO DE AMOR");
+app.MapGet("/FINAL", () => "CAMISA VERDE BRANCA E A BANDEIRA AO VENTO");
+
 
 app.Run();
+
+// - Criar um endpoint para receber informação pela URL
+// - Criar um endpoint para receber informação pelo corpo
